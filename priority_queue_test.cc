a1566112639561948e2c93d1546200c3561623ab
hakuma-matata <51562333+hakuma-matata@users.noreply.github.com>
2019-06-12 14:50:59 +0900

    Update priority_queue_test.cc
    불필요한 comment 삭제

--- priority_queue_test.cc
+++ priority_queue_test.cc
@@ -40,14 +40,6 @@ TEST(PriorityQueueTest,Trivial){
 	
 	ASSERT_TRUE(expTarget.empty());
 	
-	/* Fail Example - If you activate this code, Test will be fail *
-
-	expTarget.pop();
-	
-	EXPECT_TRUE(expTarget.empty()); // non-Fatal Failure - not terminate this test case when result of empty method is false.
-	ASSERT_EQ(expTarget.size(),0);
-        /**/
-	
 }
 
 int main(int argc, char **argv) {
